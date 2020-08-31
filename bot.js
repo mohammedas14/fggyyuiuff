861ae6bbddee7d1f3d113f473b3f5cdae440ae71
mohammedas14 <70438720+mohammedas14@users.noreply.github.com>
2020-08-31 09:23:31 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,273 @@
+//✠▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ☢❦۞❦☢ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬✠//BY: VZX (Angry Black)//POULAZ
+
+//copyright © Angry Black 2020.//❦Angry Black❦
+const Discord = require("discord.js");//Angry Black
+const myid = ['Your ID'];//Angry Black هنا بتحط الايدى تبع حسابك و هو رقم طول
+const client = new Discord.Client();//Angry Black
+const client2 = new Discord.Client();//Angry Black
+const client3 = new Discord.Client();//Angry Black
+const client4 = new Discord.Client();//Angry Black
+const client5 = new Discord.Client();//Angry Black
+const client6 = new Discord.Client();//Angry Black
+//copyright © Angry Black 2020.//❦Angry Black❦
+
+//✠▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ☢❦۞❦☢ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬✠//BY: VZX (Angry Black)//POULAZ
+
+//copyright © Angry Black 2020.//❦Angry Black❦
+client.login(process.env.TOKEN);//Angry Black
+client2.login(process.env.TOKEN2);//Angry Black
+client3.login(process.env.TOKEN3);//Angry Black
+client4.login(process.env.TOKEN4);//Angry Black
+client5.login(process.env.TOKEN5);//Angry Black
+client6.login(process.env.TOKEN6);//Angry Black
+//copyright © Angry Black 2020.//❦Angry Black❦
+
+//✠▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ☢❦۞❦☢ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬✠//BY: VZX (Angry Black)//POULAZ
+
+//copyright © Angry Black 2020.//❦Angry Black❦
+client.on('message', message => {
+  if (message.content === 'd1') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c1'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r1'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p1') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s1")) {
+//copyright © Angry Black 2020.//❦Angry Black❦
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+ 
+client2.on('message', message => {
+  if (message.content === 'd2') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c2'){
+message.channel.send('#credits')
+  }
+   if(message.content === 'r2'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p2') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s2")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+ 
+client3.on('message', message => {
+  if (message.content === 'd3') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c3'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r3'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p3') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s3")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+ 
+client4.on('message', message => {
+  if (message.content === 'd4') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c4'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r4'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p4') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s4")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+ 
+client5.on('message', message => {
+  if (message.content === 'd5') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c5'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r5'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p5') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s5")) {
+//copyright © Angry Black 2020.//❦Angry Black❦
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+
+client6.on('message', message => {
+  if (message.content === 'd6') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c6'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r6'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p6') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s6")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+//copyright © Angry Black 2020.//❦Angry Black❦
+
+//✠▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ☢❦۞❦☢ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬✠//BY: VZX (Angry Black)//POULAZ
+
+//copyright © Angry Black 2020.//❦Angry Black❦
+client.on('message', message => {
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``1`` ")//Angry Black
+    }
+});//Angry Black
+
+client2.on('message', message => {
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``2`` ")//Angry Black
+    }
+});//Angry Black
+
+client3.on('message', message => {
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``3`` ")//Angry Black
+    }
+});//Angry Black
+
+client4.on('message', message => {
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``4`` ")//Angry Black
+    }
+});//Angry Black
+
+client5.on('message', message => {
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``5`` ")//Angry Black
+    }
+});//Angry Black
+
+client6.on('message', message => {
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``6`` ")//Angry Black
+    }
+});//Angry Black
+//copyright © Angry Black 2020.//❦Angry Black❦
+
+//✠▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ☢❦۞❦☢ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬✠//BY: VZX (Angry Black)//POULAZ
+
+//copyright © Angry Black 2020.//❦Angry Black❦
+client.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? **[ " ${x} " ]`)//Angry Black
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});//Angry Black
+
+client2.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`??? ???? ??? ?? ??? **[ " ${x} " ]`)//Angry Black
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});//Angry Black
+
+client3.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)//Angry Black
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client4.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)//Angry Black
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});//Angry Black
+
+client5.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)//Angry Black
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client6.on('message', message => {
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)//Angry Black
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+//copyright © Angry Black 2020.//❦Angry Black❦
+
+//✠▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ☢❦۞❦☢ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬✠//BY: VZX (Angry Black)//POULAZ
