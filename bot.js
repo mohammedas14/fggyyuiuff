f727b3844f2be8a105e3aaae3d84f0666254a1a1
mohammedas14 <70438720+mohammedas14@users.noreply.github.com>
2020-08-31 20:26:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@
 //copyright © Angry Black 2020.//❦Angry Black❦
 const Discord = require("discord.js");//Angry Black
 const myid = ['716946561929838632'];//Angry Black هنا بتحط الايدى تبع حسابك و هو رقم طول
+const prefix = ['$'];
 const client = new Discord.Client();//Angry Black
 const client2 = new Discord.Client();//Angry Black
 const client3 = new Discord.Client();//Angry Black
